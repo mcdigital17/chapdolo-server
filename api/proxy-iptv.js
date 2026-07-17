a71c01219439afca1732eb65041fdc25f0955d81
mcdigital17 <mcdigital2214@gmail.com>
2026-07-17 12:35:50 +0200

    Update proxy-iptv.js

--- api/proxy-iptv.js
+++ api/proxy-iptv.js
@@ -3,7 +3,11 @@ export default async function handler(req, res) {
   if (!url) return res.status(400).send('URL manquante');
   
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+      }
+    });
     const text = await response.text();
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).send(text);
