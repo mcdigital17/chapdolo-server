10b7142279c3881a91910eb476daba985d8d400c
mcdigital17 <mcdigital2214@gmail.com>
2026-07-29 19:10:39 +0100

    Update proxy-bundle.js

--- api/proxy-bundle.js
+++ api/proxy-bundle.js
@@ -4,20 +4,20 @@ module.exports = async (req, res) => {
   if (req.method === 'POST') {
     body = req.body;
     if (typeof body === 'string') { try { body = JSON.parse(body); } catch (e) { return res.status(400).json({ error: 'Invalid JSON' }); } }
-    body.extra = body.extra || {};
-    body.extra.skip = body.extra.skip || 0;
-    body.extra.limit = body.extra.limit || 20;
   } else if (req.method === 'GET') {
-    const { action, type, id, sort } = req.query;
     body = {
-      action: action || 'catalog',
-      type: type || 'movie',
-      id: id || 'tmdb.movie',
-      extra: { 
-        skip: 0,
-        limit: 20,
-        sort: sort || 'popularity' 
-      }
+      "jsonrpc": "2.0",
+      "method": "catalog",
+      "params": {
+        "type": "movie",
+        "id": "tmdb.movie",
+        "extra": {
+          "skip": 0,
+          "limit": 20,
+          "sort": "popularity"
+        }
+      },
+      "id": 1
     };
   } else {
     return res.status(405).json({ error: 'Method Not Allowed' });
@@ -30,7 +30,9 @@ module.exports = async (req, res) => {
         'Content-Type': 'application/json',
         'User-Agent': 'Watched/2.3.1 (Android)',
         'Accept': 'application/json',
-        'X-SDK-Version': '2.3.1'
+        'X-SDK-Version': '2.3.1',
+        'Origin': 'https://www.huhu.to',
+        'Referer': 'https://www.huhu.to/'
       },
       body: JSON.stringify(body)
     });
