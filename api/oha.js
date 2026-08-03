7b7b59a54a900c35fed30b74a26fe402c6a1c4ae
mcdigital17 <mcdigital2214@gmail.com>
2026-08-03 15:14:43 +0100

    Update oha.js

--- api/oha.js
+++ api/oha.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   // On dit à Vercel qu'on renvoie du code Javascript (et non du HTML ou du JSON)
   res.setHeader('Content-Type', 'application/javascript');
   
