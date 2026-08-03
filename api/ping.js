9a83455edbdc98087608736e45c522eee41ccacd
mcdigital17 <mcdigital2214@gmail.com>
2026-08-03 15:15:20 +0100

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = (req, res) => {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
   res.setHeader('Cache-Control', 'no-store');
   let body;
