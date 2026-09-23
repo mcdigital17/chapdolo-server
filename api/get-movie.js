069d486435fc429f9b8014ba9873b17389ac2441
mcdigital17 <mcdigital2214@gmail.com>
2026-09-23 16:05:52 +0200

    Update get-movie.js

--- api/get-movie.js
+++ api/get-movie.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
     if (action === 'get_live_tv') {
         const { cursor } = req.query;
         try {
-            let url = `https://huhu.to/live/catalog/live/channels.json?region=FR&language=fr&sort=trending`;
+            let url = `https://huhu.to/live/catalog/channels.json?region=FR&language=fr&sort=trending`;
             if (cursor) url += `&cursor=${cursor}`;
             
             const response = await fetch(url, {
